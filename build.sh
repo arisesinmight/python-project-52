8d647c04ffda0f729163ae59b20d01705745129c
Pavel Ilchenko <arisesinmight@gmail.com>
2025-01-08 20:32:42 +0300

    try fix build

--- build.sh
+++ build.sh
@@ -2,13 +2,11 @@
 # Exit on error
 set -o errexit
 
-curl -LsSf https://astral.sh/uv/install.sh | sh
-
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-uv pip install -r requirements.txt
+pip install -r requirements.txt
 
 # Convert static asset files
-uv run python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-uv run python manage.py migrate
+python manage.py migrate
