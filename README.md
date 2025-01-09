b9f06f4e3a8b23c25a8ed56d8429d57a9c09f0df
Pavel Ilchenko <arisesinmight@gmail.com>
2025-01-10 00:31:49 +0300

    add translations

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/arisesinmight/python-project-52/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/arisesinmight/python-project-52/actions)
+
+https://python-project-52-rgvw.onrender.com/
--- task_manager/settings.py
+++ task_manager/settings.py
@@ -89,19 +89,19 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
         default='postgresql://postgres:postgres@localhost:5432/mysite',
         conn_max_age=600
     )
-}
-'''DATABASES = {
+}'''
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}'''
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -124,12 +124,15 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
+LOCALE_PATHS = ( os.path.join(BASE_DIR, 'locale'), )
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
+
+USE_L10N = True
 
 USE_TZ = True
 
